a05e7e3d875775de48d165ac403adc28de13be47
michelangelo truffarelli <michelangelo.truffarelli@outlook.com>
2021-03-02 19:48:24 +0100

    Add project files.

---
+++ Es1DesignPattern/Cella.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es1DesignPattern
+{
+    public class Cella
+    {
+        internal Cella() { }
+    }
+}
---
+++ Es1DesignPattern/Riga.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es1DesignPattern
+{
+    public class Riga
+    {
+        List<Cella> celle = new List<Cella>();
+
+        internal Riga() { }
+
+        public Cella NuovaCella()
+        {
+            Cella c = new Cella();
+            celle.Add(c);
+            return c;
+        }
+    }
+}
